9ca265e157390e28f9eab2e1cb8e939f0808c3b9
Saim Ahmed <e3saim@mail.com>
2024-06-16 04:35:13 -0700

    Deploy fix

--- youth/settings.py
+++ youth/settings.py
@@ -80,11 +80,11 @@ WSGI_APPLICATION = 'youth.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ["PGDATABASE"],
-        'USER': os.environ["PGUSER"],
-        'PASSWORD': os.environ["PGPASSWORD"],
-        'HOST': os.environ["PGHOST"],
-        'PORT': os.environ["PGPORT"],
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'kGdoSHfIEgryRxwdVuFMKHGPHhgIwSRJ',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': 40063,
     }
 }
 
